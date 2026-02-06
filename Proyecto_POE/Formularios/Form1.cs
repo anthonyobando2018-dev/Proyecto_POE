a1f5192d903af7b1090d2255a7197144d02000ee
antoniobando2018-dev <anthonyobando2018@gmail.com>
2026-02-06 13:23:47 -0500

    le AGREGUE COLORES ALOS BOTONES DEL FRM PRINCIPAL

--- Proyecto_POE/Formularios/Form1.cs
+++ Proyecto_POE/Formularios/Form1.cs
@@ -14,8 +14,10 @@ namespace Proyecto_POE
 
         private void btnPrincipal_Click(object sender, EventArgs e)
         {
-            btnPrincipal.ForeColor = Color.White;
-            btnPrincipal.FillColor = Color.FromArgb(21, 93, 252);
+            //btnPrincipal.ForeColor = Color.White;
+            //btnPrincipal.FillColor = Color.FromArgb(21, 93, 252);
+            ResetearBotones();
+            ActivarBoton(btnPrincipal);
             FrmPrincipal frm = new FrmPrincipal();
             AbrirFormEnPanel(frm);
         }
@@ -34,34 +36,72 @@ namespace Proyecto_POE
 
         private void btnLaboratorio_Click(object sender, EventArgs e)
         {
-
+            ResetearBotones();
+            ActivarBoton(btnLaboratorio);
             FrmLaboratorio frm = new FrmLaboratorio();
             AbrirFormEnPanel(frm);
         }
 
         private void btnDocentes_Click(object sender, EventArgs e)
         {
+            ResetearBotones();
+            ActivarBoton(btnDocentes);
             FrmDocente frm = new FrmDocente();
             AbrirFormEnPanel(frm);
         }
 
         private void btnReservas_Click(object sender, EventArgs e)
         {
+            ResetearBotones();
+            ActivarBoton(btnReservas);
             FrmReservas frm = new FrmReservas();
             AbrirFormEnPanel(frm);
         }
 
         private void btnConsultas_Click(object sender, EventArgs e)
         {
+            ResetearBotones();
+            ActivarBoton(btnConsultas);
             FrmConsultas frm = new FrmConsultas();
             AbrirFormEnPanel(frm);
         }
 
         private void btnReportes_Click(object sender, EventArgs e)
         {
+            ResetearBotones();
+            ActivarBoton(btnReportes);
             FrmReportes frm = new FrmReportes();
             AbrirFormEnPanel(frm);
         }
+
+        private void ResetearBotones()
+        {
+            btnPrincipal.ForeColor = Color.Black;
+            btnPrincipal.FillColor = Color.White;
+
+            btnLaboratorio.ForeColor = Color.Black;
+            btnLaboratorio.FillColor = Color.White;
+
+            btnDocentes.ForeColor = Color.Black;
+            btnDocentes.FillColor = Color.White;
+
+            btnReservas.ForeColor = Color.Black;
+            btnReservas.FillColor = Color.White;
+
+            btnConsultas.ForeColor = Color.Black;
+            btnConsultas.FillColor = Color.White;
+
+            btnReportes.ForeColor = Color.Black;
+            btnReportes.FillColor = Color.White;
+        }
+
+        private void ActivarBoton(Guna.UI2.WinForms.Guna2Button boton)
+        {
+            boton.ForeColor = Color.White;
+            boton.FillColor = Color.FromArgb(21, 93, 252);
+        }
+
     }
+
 
 }
