a707a46370a03fb2db9a78b4cf6fefeb8f006a66
antoniobando2018-dev <anthonyobando2018@gmail.com>
2026-02-05 23:14:19 -0500

    nueva Rama

--- Proyecto_POE/Formularios/FrmDocente.Designer.cs
+++ Proyecto_POE/Formularios/FrmDocente.Designer.cs
@@ -28,18 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.CustomizableEdges = customizableEdges1;
+            guna2Panel1.Location = new Point(250, 94);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2Panel1.Size = new Size(200, 100);
+            guna2Panel1.TabIndex = 0;
             // 
             // FrmDocente
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1002, 589);
+            Controls.Add(guna2Panel1);
             Name = "FrmDocente";
             Text = "FrmDocente";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
     }
 }
